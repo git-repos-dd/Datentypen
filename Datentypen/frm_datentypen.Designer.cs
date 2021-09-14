610c4292292da6ded32db7e3fc97436e0efead02
Dirk Burggraf <dirk.aus.dresden@gmx.de>
2021-09-14 10:12:05 +0200

    Projektdateien hinzufügen.

---
+++ Datentypen/frm_datentypen.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Datentypen
+{
+    partial class frm_datentypen
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_datentypen_anzeigen = new System.Windows.Forms.Button();
+            this.lbl_datentypen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_datentypen_anzeigen
+            // 
+            this.btn_datentypen_anzeigen.Location = new System.Drawing.Point(12, 12);
+            this.btn_datentypen_anzeigen.Name = "btn_datentypen_anzeigen";
+            this.btn_datentypen_anzeigen.Size = new System.Drawing.Size(138, 23);
+            this.btn_datentypen_anzeigen.TabIndex = 0;
+            this.btn_datentypen_anzeigen.Text = "Datentypen anzeigen";
+            this.btn_datentypen_anzeigen.UseVisualStyleBackColor = true;
+            this.btn_datentypen_anzeigen.Click += new System.EventHandler(this.btn_datentypen_anzeigen_Click);
+            // 
+            // lbl_datentypen
+            // 
+            this.lbl_datentypen.AutoSize = true;
+            this.lbl_datentypen.Location = new System.Drawing.Point(13, 42);
+            this.lbl_datentypen.Name = "lbl_datentypen";
+            this.lbl_datentypen.Size = new System.Drawing.Size(68, 13);
+            this.lbl_datentypen.TabIndex = 1;
+            this.lbl_datentypen.Text = "... Platzhalter";
+            // 
+            // frm_datentypen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_datentypen);
+            this.Controls.Add(this.btn_datentypen_anzeigen);
+            this.Name = "frm_datentypen";
+            this.Text = "Datentypen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_datentypen_anzeigen;
+        private System.Windows.Forms.Label lbl_datentypen;
+    }
+}
+
---
+++ Datentypen/frm_datentypen.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datentypen
+{
+    public partial class frm_datentypen : Form
+    {
+        public frm_datentypen()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_datentypen_anzeigen_Click(object sender, EventArgs e)
+        {
+            /* Ganze Zahlen */
+            byte By;
+            short Sh;
+            int It, Hex;
+            long Lg;
+            /* Zahlen mit Nachkommastellen */
+            float Fl;
+            double Db1, Db2, Exp1, Exp2;
+            decimal De;
+            /* Boolesche Variable, Zeichen, Zeichenkette */
+            bool Bo;
+            char Ch;
+            string St;
+            /* Ganze Zahlen */
+            By = 200;
+            Sh = 30000;
+            It = 2000000000;
+            Lg = 3000000000;
+            Hex = 0x3a;
+            /* Zahlen mit Nachkommastellen */
+            Fl = 1.0f / 7;
+            Db1 = 1 / 7;
+            Db2 = 1.0 / 7;
+            De = 1.0m / 7;
+            Exp1 = 1.5e3;
+            Exp2 = 1.5e-3;
+            /* Boolesche Variable, Zeichen, Zeichenkette */
+            Bo = true;
+            Ch = 'a';
+            St = "Zeichenkette";
+            lbl_datentypen.Text =
+                "byte: " +By + "\n" + "short: " +Sh + "\n" +
+                "int: " +It + "\n" + "long: " +Lg + "\n" +
+                "(hexadezimale Zahl): " +Hex + "\n\n" +
+                "float: " +Fl + "\n" + "double 1: " +Db1 + "\n" +
+                "double 2: " +Db2 + "\n" + "decimal: " +De + "\n" +
+                "(Exponent positiv): " +Exp1 + "\n" +
+                "(Exponent negativ): " +Exp2 + "\n\n" +
+                "bool: " +Bo + "\n" + "char: " +Ch + "\n" + "string: " +St;
+        }
+    }
+}
